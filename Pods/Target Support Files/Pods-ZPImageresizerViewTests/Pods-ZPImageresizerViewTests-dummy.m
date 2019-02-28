9a84149b2233d3e9a13a043765f8d9a89b73414d
Justin <1272524311@qq.com>
2019-02-28 17:32:57 +0800

    fjjdfj

---
+++ Pods/Target Support Files/Pods-ZPImageresizerViewTests/Pods-ZPImageresizerViewTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZPImageresizerViewTests : NSObject
+@end
+@implementation PodsDummy_Pods_ZPImageresizerViewTests
+@end
---
+++ Pods/Target Support Files/Pods-ZPImageresizerViewUITests/Pods-ZPImageresizerViewUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZPImageresizerViewUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ZPImageresizerViewUITests
+@end
